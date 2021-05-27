5f8b9feea42c10e573562197776a1c8151b1da5b
AP\CSchulz1 <cornelia_schulz@trimble.com>
2021-05-27 15:17:37 +1200

    first commit

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new { id = 1, Name = "Auckland" },
+                    new { id = 2, Name = "Sydney" }
+                });
+        }
+    }
+}
