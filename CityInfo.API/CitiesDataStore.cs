8dd4e1339271bf0a47f3569ef32d478e69fe0dba
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2021-05-28 11:51:03 +1200

    add point of interest model and controller

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -19,19 +19,64 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "Auckland",
-                    Description = "The big smoke"
+                    Description = "The big smoke",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Skytower",
+                            Description = "Big tower in the centre of town that you can have dinner at or jump off."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Mount Eden",
+                            Description = "Volcano with awesome views over the whole of Auckland."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Sydney",
-                    Description = "The one with the famous Opera House"
+                    Description = "The one with the famous Opera House",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Opera House",
+                            Description = "Opera House in the shape of a sail."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Darling Harbour",
+                            Description = "Great area to stroll around and take in the sights."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "Paris",
-                    Description = "The one with the big metal tower"
+                    Description = "The one with the big metal tower",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Eiffel Tower",
+                            Description = "Big tower in the centre of the city."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Louvre",
+                            Description = "Fantastic Museum full of art."
+                        }
+                    }
                 }
             };
         }
